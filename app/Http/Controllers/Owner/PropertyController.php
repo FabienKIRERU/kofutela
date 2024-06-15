0238cf8b3e6b77cca07ea3093da399ab3541b7a1
FabienKIRERU <100976153+FabienKIRERU@users.noreply.github.com>
2024-06-15 20:02:21 +0200

    Update PropertyController.php

--- app/Http/Controllers/Owner/PropertyController.php
+++ app/Http/Controllers/Owner/PropertyController.php
@@ -135,7 +135,7 @@ class PropertyController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(int $id)
+    public function destroy(Request $id)
     {
         Property::destroy($id);
         // $property->delete();
