b173594bb9c41858c576be3f7efe3eba8e1c63ff
FabienKIRERU <100976153+FabienKIRERU@users.noreply.github.com>
2024-06-19 18:21:26 +0200

    Update card.blade.php

--- resources/views/property/card.blade.php
+++ resources/views/property/card.blade.php
@@ -1,9 +1,19 @@
-<div class="card m-2">
+<style>
+    .card{
+        height: 80vh;
+    }
+    @media screen and (max-width: 687px){
+        .card{
+            height: 70vh;
+        }
+    }
+</style>
+<div class="card " >
     @if ($property->getPicture())
-        <img src="{{ $property->getPicture()->getImageUrl(360, 230) }}" alt="" class="w-100">
+        <img src="{{ $property->getPicture()->getImageUrl(360, 230) }}" alt="" >
     @else
-    <small class="text-danger"> ce bien n'a pas de photo </small>
-    <img src="{{ asset('logo/papabailleur.png') }}" alt="" width="230px"  class="d-flex">
+        <small class="text-danger"> ce bien n'a pas de photo </small>
+        <img src="{{ asset('logo/papabailleur.png') }}" alt=""  >
     @endif
     <div class="card-body">
         <div class="badge bg-danger">{{$property->category?->titre}}</div><br>
@@ -11,17 +21,17 @@
         <h5 class="card-title">
             <a href="{{route('property.show', ['slug'=> $property->getSlug(), 'property' => $property])}}" >{{$property->title}}</a>
         </h5>
-        <p class="card-text">
-            {{$property->surface}} m²  
+        <p class="text-secondary" >
+            surface: {{$property->surface}} m²  
         </p>
-        <p class="card-text">
-            C/ {{$property->quarter->area->name}}  <br>
-            Q/ {{$property->quarter->name}} <br>
-            {{$property->address}} -{{$property->city}}
-        </p>
-        <div class="text-danger fw-bold" style="font-size:1em">
+        <p class="text-secondary fw-bold" >
+            Garantie: {{ number_format($property?->garanteed, thousands_separator: ' ') }} $ <br>
             Prix mensuel: {{ number_format($property->price, thousands_separator: ' ') }} $
-            Garantie: {{ number_format($property?->garanteed, thousands_separator: ' ') }} $
-        </div>
+        </p>
+        <p class="card-text text-secondary">
+            <i class="fa-solid fa-home" style="font-size: 15px; color: rgba(66, 66, 66, 0.418)"></i> {{$property->quarter->area->name}}
+            ,{{$property->quarter->name}}
+            ,{{$property->address}} -{{$property->city}}
+        </p>
     </div>
 </div>
