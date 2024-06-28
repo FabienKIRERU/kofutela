cb695b4c6dd470ab5156a7ad5b963fd9dca4f026
FabienKIRERU <100976153+FabienKIRERU@users.noreply.github.com>
2024-06-28 12:06:56 +0200

    Update card.blade.php

--- resources/views/property/card.blade.php
+++ resources/views/property/card.blade.php
@@ -15,18 +15,16 @@
         <small class="text-danger"> ce bien n'a pas de photo </small>
         <img src="{{ asset('logo/papabailleur.png') }}" alt=""  >
     @endif
-    <div class="card-body">
+    <div class="card-body text-secondary">
         <div class="badge bg-danger">{{$property->category?->titre}}</div><br>
         <div class="badge bg-dark"> Propriétaire: {{$property->user?->username}}</div>
         <h5 class="card-title">
             <a href="{{route('property.show', ['slug'=> $property->getSlug(), 'property' => $property])}}" >{{$property->title}}</a>
         </h5>
         <p class="text-secondary" >
-            <i class="fa-regular fa-square-full" style="font-size: 15px; color: rgba(255, 0, 0, 0.596)"></i> {{$property->surface}} m²  
-        </p>
-        <p class="text-secondary fw-bold" >
-            Garantie: {{ number_format($property?->garanteed, thousands_separator: ' ') }} $ <br>
-            Prix mensuel: {{ number_format($property->price, thousands_separator: ' ') }} $
+            <i class="fa-regular fa-square-full" style="font-size: 15px; color: rgba(255, 0, 0, 0.596)"></i> {{$property->surface}} m²  | 
+            <small>Garantie:</small> {{ number_format($property?->garanteed, thousands_separator: ' ') }} $ |
+            <small>Prix mensuel:</small> {{ number_format($property->price, thousands_separator: ' ') }} $
         </p>
         <p class="card-text text-secondary">
             <i class="fa-solid fa-location-dot"  style="font-size: 20px; color: rgba(255, 0, 0, 0.596)"> </i> {{$property->quarter->area->name}}
