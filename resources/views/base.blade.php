9e174dd5bee41d270196517272dc6bf8636eaa03
FabienKIRERU <100976153+FabienKIRERU@users.noreply.github.com>
2024-08-03 02:59:01 +0200

    Update base.blade.php

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,6 +8,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
+    <link rel="shortcut icon" href="{{asset('logo/papabailleur.png')}}" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
 
     <link rel="stylesheet" href="{{asset('fontawesome-free-6.5.2-web/fontawesome-free-6.5.2-web/css/all.min.css')}}">
